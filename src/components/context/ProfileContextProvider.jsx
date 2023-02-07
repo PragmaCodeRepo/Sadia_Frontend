d92f330b617ce654f790ac8ba226a0e3a1d2fc3e
PragmaCodeRepo <chaubeypranjal027@gmail.com>
2023-02-07 15:04:49 +0530

    hh

--- src/components/context/ProfileContextProvider.jsx
+++ src/components/context/ProfileContextProvider.jsx
@@ -5,8 +5,8 @@ const ProfileContextProvider = (props) => {
   const [profiles, setProfiles] = useState([]);
   const [isFound, setIsFound] = useState(false);
   const profileData = async () => {
-    // const response = await fetch("http://192.241.128.245:8000/api/Profiles/");
-    const response = await fetch("http://pragma.how:8000/api/Profiles/");
+    const response = await fetch("http://192.241.128.245:8000/api/Profiles/");
+    // const response = await fetch("http://pragma.how:8000/api/Profiles/");
     // const response=await fetch("http://127.0.0.1:8000/api/Profiles/");
 
     // const response=await fetch("http://192.241.128.245/:8000/api/Profiles/");
