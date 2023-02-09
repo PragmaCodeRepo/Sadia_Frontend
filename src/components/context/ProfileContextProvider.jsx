10294c63410d1aad8286a2943b640a5ba46697ba
PragmaCodeRepo <chaubeypranjal027@gmail.com>
2023-02-09 11:51:41 +0530

    change is profile context

--- src/components/context/ProfileContextProvider.jsx
+++ src/components/context/ProfileContextProvider.jsx
@@ -5,6 +5,7 @@ const ProfileContextProvider = (props) => {
   const [profiles, setProfiles] = useState([]);
   const [isFound, setIsFound] = useState(false);
   const profileData = async () => {
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/> 
     const response = await fetch("http://192.241.128.245/api/Profiles/");
     // const response = await fetch("http://pragma.how:8000/api/Profiles/");
     // const response=await fetch("http://127.0.0.1:8000/api/Profiles/");
