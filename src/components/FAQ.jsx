f43abe206d343ba2cb7d70d2bd66801c1bfa51d3
PragmaCodeRepo <chaubeypranjal027@gmail.com>
2023-03-02 17:34:09 +0530

    adding faq

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -19,34 +19,40 @@ const FAQ = () => {
 
           <details class="faq-card">
             <summary>
-              How to move on after divorce? <span class="faq-open-icon">+</span>
+            What is divorce mediation? <span class="faq-open-icon">+</span>
             </summary>
-            <span class="faq-card-spoiler">coming soon</span>
+            <span class="faq-card-spoiler">Divorce mediation is a process in which a neutral third party, the mediator, helps couples negotiate and reach agreements on various aspects of their divorce, such as property division, child custody, and spousal support.</span>
           </details>
           <details class="faq-card">
             <summary>
-              How to contact resouces? <span class="faq-open-icon">+</span>
+            What are the benefits of divorce mediation? <span class="faq-open-icon">+</span>
             </summary>
-            <span class="faq-card-spoiler">coming soon</span>
+            <span class="faq-card-spoiler">Some benefits of divorce mediation include lower costs, quicker resolution, reduced conflict, and more control over the outcome of the divorce.</span>
           </details>
           <details class="faq-card">
             <summary>
-              How to manage child after divorce{" "}
+            What is a collaborative divorce?{" "}
               <span class="faq-open-icon">+</span>
             </summary>
-            <span class="faq-card-spoiler">coming soon</span>
+            <span class="faq-card-spoiler"> A collaborative divorce is a process in which both spouses work with a team of professionals, including attorneys, financial experts, and mental health professionals, to negotiate a divorce settlement outside of court.</span>
           </details>
           <details class="faq-card">
             <summary>
-              How to move on after divorce? <span class="faq-open-icon">+</span>
+            What is a parenting plan? <span class="faq-open-icon">+</span>
             </summary>
-            <span class="faq-card-spoiler">coming soon</span>
+            <span class="faq-card-spoiler">A parenting plan is a written agreement that outlines how divorced or separated parents will share custody and make decisions about their children's upbringing.</span>
           </details>
           <details class="faq-card">
             <summary>
-              How to move on after divorce? <span class="faq-open-icon">+</span>
+            What is child support? <span class="faq-open-icon">+</span>
             </summary>
-            <span class="faq-card-spoiler">coming soon</span>
+            <span class="faq-card-spoiler">Child support is a court-ordered payment that a non-custodial parent must make to the custodial parent to help cover the costs of raising their children.</span>
+          </details>
+          <details class="faq-card">
+            <summary>
+            How is child support calculated?<span class="faq-open-icon">+</span>
+            </summary>
+            <span class="faq-card-spoiler">Child support is typically calculated based on a formula that takes into account factors such as each parent's income, the number of children, and the amount of time each parent spends with the children.</span>
           </details>
 
           <h2 class="faq-heading">Still have questions?</h2>
